910f251980e6d929fcaf237891f33dad6d364fc3
tiago <tiago.spana@gmail.com>
2021-10-20 21:18:02 -0300

    feat: base64 enconde

---
+++ Helpfy/Base64Extensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Helpfy
+{
+    public static class Base64Extensions
+    {
+        public static string ToBase64(string plainText) => new Base64Code().Encode(plainText);
+    }
+    public class Base64Code
+    {
+        private const string Base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/+=";
+        public string Encode(string plainText)
+        {
+            var bytes = Encoding.UTF8.GetBytes(plainText);
+            StringBuilder sb = new StringBuilder();
+            var binaries = GetBinaries(bytes);
+            foreach (var item in binaries)
+            {
+                var split = Regex.Split(item, @"(\d{6})").Where(r => !string.IsNullOrEmpty(r)).ToArray();
+                if (split.Length == 4)
+                {
+                    foreach (var binary in split)
+                    {
+                        var value = '=';
+                        if (binary != "000000")
+                        {
+                            var index = Convert.ToInt32(binary, 2);
+                            value = Base64[index];
+
+                        }
+                        sb.Append(value);
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        private List<string> GetBinaries(byte[] bytes)
+        {
+            var list = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            bytes = fillBytes(bytes);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+
+                sb.Append(Convert.ToString(bytes[i], 2).PadLeft(8, '0'));
+                if ((i + 1) % 3 == 0)
+                {
+                    list.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+            return list;
+        }
+
+        private byte[] fillBytes(byte[] bytes)
+        {
+            if (bytes.Length % 3 != 0)
+            {
+                var newArray = new byte[bytes.Length + (3 - (bytes.Length % 3))];
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    var value = bytes.Length > i ? bytes[i] : (byte)0;
+                    newArray[i] = value;
+                }
+                return newArray;
+            }
+            return bytes;
+
+        }
+    }
+}
---
+++ HelpfyTests/Base64CodeTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helpfy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpfy.Tests
+{
+    [TestClass()]
+    public class Base64CodeTests
+    {
+        [TestMethod()]
+        public void EncodeTest()
+        {
+            Base64Code base64 = new Base64Code();
+            Assert.IsNotNull(base64);
+            Assert.AreEqual("T2zDoSwgbXVuZG8h", base64.Encode("Olá, mundo!"));
+        }
+        [TestMethod()]
+        public void LeviathanFragment()
+        {
+            Base64Code base64 = new Base64Code();
+            Assert.IsNotNull(base64);
+            Assert.AreEqual("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=", base64.Encode(@"
+Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.".Trim()));
+        }
+        [TestMethod()]
+        public void MaTest()
+        {
+            Base64Code base64 = new Base64Code();
+            Assert.IsNotNull(base64);
+            Assert.AreEqual("TWE=", base64.Encode("Ma"));
+        }
+    }
+}
